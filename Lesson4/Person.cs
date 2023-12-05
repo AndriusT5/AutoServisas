8a02e694add261231f50dfa91de126bc348c3855
Andrius Zvinakevicius <ext-andrius.zvinakevicius@aktia.fi>
2023-12-05 19:28:37 +0200

    New feature implemented
    Added Bike and Cars support

---
+++ Lesson4/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    public class Person
+    {
+        int PesonalCode;
+        public string Name;
+        public string Surname;
+        public int Age;
+        public List<Car> OwnedCars = new List<Car>();
+        public List<Bike> OwnedBikes = new List<Bike>();
+
+        public void BuyCar(Car car)
+        {
+            OwnedCars.Add(car);
+            Console.WriteLine($"Congrats with new car {car.Make}");
+        }
+
+        public void BuyBike(Bike bike)
+        {
+            OwnedBikes.Add(bike);
+            Console.WriteLine($"Congrats with new car {bike.Make}");
+        }
+    }
+}
---
+++ Lesson4/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    public class Vehicle
+    {
+        public string Make;
+        public string Model;
+        public int MadeYear;
+        public string Color;
+        public bool IsBroken;
+        public bool TechValid;
+
+        public void StartEngine()
+        {
+            Console.WriteLine($"{Make} Engine started was started;");
+        }
+    }
+}
